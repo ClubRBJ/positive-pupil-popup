3acf6268aa79d94247bbd289b5c88dff93fd0379
leiaar <90118155+leiaar@users.noreply.github.com>
2023-02-14 14:14:12 -0500

    Working Popup Button

--- js/button.js
+++ js/button.js
@@ -1,3 +1,4 @@
 document.getElementById('change-text').addEventListener("click", function() { 
     document.getElementById('change-text').textContent = 'hello';
+    window.open("https://www.google.com");
 });
