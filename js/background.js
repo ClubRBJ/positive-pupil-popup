adb64b52a536a49aff1eac5537f2facd93fc86bc
Ben Schwartz <btschwartz12@gmail.com>
2023-03-07 20:50:26 -0500

    Refactored some more

--- js/background.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-    console.log("Message received: ", message);
-    if (message.action === "openPopup") {
-      chrome.action.openPopup();
-      sendResponse({ message: "Popup opened" });
-    }
-  });
-  
-  
